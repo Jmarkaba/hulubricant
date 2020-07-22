52f636ee79ad2c747e2ea9eb092b6351654ea28b
Jah Markabawi <jmarkaba@asu.edu>
2020-07-21 18:41:58 -0700

    Blocks descriptions in other locations

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ A simple Firefox extension to get rid of those pesky spoilers that appear right 
 the next episode of a series. It also converts all episode thumbnails to black to prevent
 any possible spoilers from those as well.
 
-A toggle for the extension will come in a future version.
+A toggle for the extension may come in a future version. For now if you wish to block the 
+features just disable the extension with FireFox.
 
 ## Insertion Query
 
--- remove-spoilers.js
+++ remove-spoilers.js
@@ -1,11 +1,9 @@
-// insertion-query v1.0.3 (2016-01-20) 
-// license:MIT 
-// Zbyszek Tenerowicz <naugtur@gmail.com> (http://naugtur.pl/) 
-var insertionQ=function(){"use strict";function a(a,b){var d,e="insQ_"+g++,f=function(a){(a.animationName===e||a[i]===e)&&(c(a.target)||b(a.target))};d=document.createElement("style"),d.innerHTML="@"+j+"keyframes "+e+" {  from {  outline: 1px solid transparent  } to {  outline: 0px solid transparent }  }\n"+a+" { animation-duration: 0.001s; animation-name: "+e+"; "+j+"animation-duration: 0.001s; "+j+"animation-name: "+e+";  } ",document.head.appendChild(d);var h=setTimeout(function(){document.addEventListener("animationstart",f,!1),document.addEventListener("MSAnimationStart",f,!1),document.addEventListener("webkitAnimationStart",f,!1)},n.timeout);return{destroy:function(){clearTimeout(h),d&&(document.head.removeChild(d),d=null),document.removeEventListener("animationstart",f),document.removeEventListener("MSAnimationStart",f),document.removeEventListener("webkitAnimationStart",f)}}}function b(a){a.QinsQ=!0}function c(a){return n.strictlyNew&&a.QinsQ===!0}function d(a){return c(a.parentNode)?a:d(a.parentNode)}function e(a){for(b(a),a=a.firstChild;a;a=a.nextSibling)void 0!==a&&1===a.nodeType&&e(a)}function f(f,g){var h=[],i=function(){var a;return function(){clearTimeout(a),a=setTimeout(function(){h.forEach(e),g(h),h=[]},10)}}();return a(f,function(a){if(!c(a)){b(a);var e=d(a);h.indexOf(e)<0&&h.push(e),i()}})}var g=100,h=!1,i="animationName",j="",k="Webkit Moz O ms Khtml".split(" "),l="",m=document.createElement("div"),n={strictlyNew:!0,timeout:20};if(m.style.animationName&&(h=!0),h===!1)for(var o=0;o<k.length;o++)if(void 0!==m.style[k[o]+"AnimationName"]){l=k[o],i=l+"AnimationName",j="-"+l.toLowerCase()+"-",h=!0;break}var p=function(b){return h&&b.match(/[^{}]/)?(n.strictlyNew&&e(document.body),{every:function(c){return a(b,c)},summary:function(a){return f(b,a)}}):!1};return p.config=function(a){for(var b in a)a.hasOwnProperty(b)&&(n[b]=a[b])},p}();"undefined"!=typeof module&&"undefined"!=typeof module.exports&&(module.exports=insertionQ);
-
 // Hulubricant
 // Jah Markabawi
-const spoilerReplacement = "Ha, you thought! No more spoilers for you.\nStop reading this or I'll poke out your eye."
+// Date: 2020-07-21
+const spoilerReplacement = "Ha, you thought! No more spoilers for you.\nStop reading this, or I'll poke out your eye.";
 insertionQ.config({strictlyNew: false});
 insertionQ(".GenericTileThumbnail__image").every(image => image.src = "https://upload.wikimedia.org/wikipedia/en/thumb/6/62/LMS_plain_black_landscape.svg/1024px-LMS_plain_black_landscape.svg.png");
 insertionQ(".end-card__metadata-area-description").every(desc => desc.textContent = spoilerReplacement);
+insertionQ(".GenericTileContent__details-item").every(p => p.hidden = true);
+insertionQ(".Masthead__description").every(div => div.hidden = true);
