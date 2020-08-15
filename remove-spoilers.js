d696a2b96b9c8d232991a3cbf0c53a933db3c866
Jah Markabawi <jmarkaba@asu.edu>
2020-08-15 12:05:57 -0700

    Remove end thumbnail

--- remove-spoilers.js
+++ remove-spoilers.js
@@ -1,9 +1,11 @@
 // Hulubricant
 // Jah Markabawi
 // Date: 2020-07-21
+const blackImageUrl = "https://upload.wikimedia.org/wikipedia/en/thumb/6/62/LMS_plain_black_landscape.svg/1024px-LMS_plain_black_landscape.svg.png";
 const spoilerReplacement = "Ha, you thought! No more spoilers for you.\nStop reading this, or I'll poke out your eye.";
 insertionQ.config({strictlyNew: false});
 insertionQ(".Masthead__description").every(div => div.hidden = true);
 insertionQ(".StandardEmphasisHorizontalTileContent__description").every(div => div.innerHTML = "");
-insertionQ(".StandardEmphasisHorizontalTileThumbnail__image").every(image => image.src = "https://upload.wikimedia.org/wikipedia/en/thumb/6/62/LMS_plain_black_landscape.svg/1024px-LMS_plain_black_landscape.svg.png");
+insertionQ(".StandardEmphasisHorizontalTileThumbnail__image").every(img => img.src = blackImageUrl);
+insertionQ(".end-card__thumbnail").every(img => img.src = blackImageUrl);
 insertionQ(".end-card__metadata-area-description").every(desc => desc.textContent = spoilerReplacement);
